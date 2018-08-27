68eada1dd43936ff963860d14d188efd2f8a45dc
opet <opet@Academico.edu>
2018-08-27 11:06:46 -0300

    27/08/2018 11h06

---
+++ app/src/main/java/com/example/opet/loginfirebase/Pessoa.java
@@ -0,0 +1,17 @@
+package com.example.opet.loginfirebase;
+
+import java.util.Date;
+
+/**
+ * Created by opet on 27/08/2018.
+ */
+
+public class Pessoa {
+    private int id;
+    private Date nascimento;
+    private String graduacao;
+    private String idioma;
+
+    public Pessoa() {
+    }
+}
