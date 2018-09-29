2fb3e995dd3a688d0bb9c88fc86daf89540393f0
Kethllyn dos Santos Martins <30841388+keeh1406@users.noreply.github.com>
2018-09-29 12:06:27 -0300

    29/09/2018 12h06

--- app/src/main/java/com/example/opet/loginfirebase/Pessoa.java
+++ app/src/main/java/com/example/opet/loginfirebase/Pessoa.java
@@ -8,10 +8,66 @@ import java.util.Date;
 
 public class Pessoa {
     private int id;
+    private String nome;
     private Date nascimento;
     private String graduacao;
     private String idioma;
 
     public Pessoa() {
+    }
+
+    public Pessoa(int id, String nome, Date nascimento, String graduacao, String idioma) {
+        this.id = id;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.graduacao = graduacao;
+        this.idioma = idioma;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getGraduacao() {
+        return graduacao;
+    }
+
+    public void setGraduacao(String graduacao) {
+        this.graduacao = graduacao;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
 }
--- app/src/main/java/com/example/opet/loginfirebase/TelaInicial.java
+++ app/src/main/java/com/example/opet/loginfirebase/TelaInicial.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,6 +27,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  * Created by opet on 21/08/2018.
  */
@@ -28,13 +37,41 @@ import com.google.firebase.database.ValueEventListener;
 public class TelaInicial extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private ProgressBar pbSalvar;
+    EditText editNome, editIdioma, editGraduacao, editNascimento;
+    ListView listV_dados;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    private List<Pessoa> listPessoa = new ArrayList<Pessoa>();
+    private ArrayAdapter<Pessoa> arrayAdapterPessoa;
+
+    Pessoa pessoaSelecionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
         pbSalvar = findViewById(R.id.pbSalvar);
+        editNome = (EditText)findViewById(R.id.editNome);
+        editIdioma = (EditText)findViewById(R.id.editIdioma);
+        editGraduacao = (EditText)findViewById(R.id.editGraduacao);
+        editNascimento = (EditText)findViewById(R.id.editNascimento);
+        listV_dados = (ListView)findViewById(R.id.listV_dados);
+
+        inicializarFirebase();
+        eventoDataBase();
+
+        listV_dados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                pessoaSelecionada = (Pessoa)parent.getItemAtPosition(i);
+                editNome.setText(pessoaSelecionada.getNome());
+                editGraduacao.setText(pessoaSelecionada.getGraduacao());
+                editIdioma.setText(pessoaSelecionada.getIdioma());
+                editNascimento.setText((CharSequence) pessoaSelecionada.getNascimento());
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -45,28 +82,83 @@ public class TelaInicial extends AppCompatActivity {
 
     }
 
-    public void salvarCarro(View view){
-        Carro c = new Carro("Audi", "A4", 2018);
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(TelaInicial.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
+        databaseReference = firebaseDatabase.getReference();
+    }
 
-        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference mRef = mDatabase.getReference("carros");
-
-        pbSalvar.setVisibility(ProgressBar.VISIBLE);
-        mRef.setValue(c);
-
-        mRef.addValueEventListener(new ValueEventListener() {
+    private void eventoDataBase()
+    {
+        databaseReference.child("Pessoa").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Toast.makeText(TelaInicial.this, "Carro salvo", Toast.LENGTH_SHORT).show();
-                pbSalvar.setVisibility(ProgressBar.GONE);
+                listPessoa.clear();
+                for (DataSnapshot objetoSnapshot:dataSnapshot.getChildren()){
+                    Pessoa p = objetoSnapshot.getValue(Pessoa.class);
+                    listPessoa.add(p);
+                }
+                arrayAdapterPessoa = new ArrayAdapter<Pessoa>(TelaInicial.this, android.R.layout.simple_list_item_1, listPessoa);
+                listV_dados.setAdapter(arrayAdapterPessoa);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(TelaInicial.this, "Erro ao salvar", Toast.LENGTH_SHORT).show();
-                pbSalvar.setVisibility(ProgressBar.GONE);
+
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if(id == R.id.menu_novo){
+            Pessoa p = new Pessoa();
+            p.setId(Integer.parseInt(UUID.randomUUID().toString()));
+            p.setNome(EditNome.getText().toString());
+            p.setIdioma(EditIdioma.getText().toString());
+            p.setGraduacao(EditGraduacao.getText().toString());
+            p.setNascimento(EditNascimento.getText().toString());
+            databaseReference.child("Pessoa").child(String.valueOf(p.getId())).setValue(p);
+            limparCampos();
+        }
+        else
+            if (id == R.id.menu_atualiza)
+            {
+                Pessoa p = new Pessoa();
+                p.setId(pessoaSelecionada.getId());
+                p.setNome(editNome.getText().toString().trim());
+                p.setNascimento(editNascimento.getText().toString().trim());
+                p.setGraduacao(editGraduacao.getText().toString().trim());
+                p.setIdioma(editIdioma.getText().toString().trim());
+                databaseReference.child("Pessoa").child(String.valueOf(p.getId())).setValue(p);
+                limparCampos();
+            }
+            else
+                if (id == R.id.menu_deleta)
+                {
+                    Pessoa p = new Pessoa();
+                    p.setId(pessoaSelecionada.getId());
+                    databaseReference.child("Pessoa").child(p.getId().removeValue());
+                }
+        return true;
+    }
+
+    private void limparCampos()
+    {
+        editNome.setText("");
+        editGraduacao.setText("");
+        editIdioma.setText("");
+        editNascimento.setText("");
     }
 
     public void Sair(View view){
